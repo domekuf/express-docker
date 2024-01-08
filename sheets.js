c56cff33cc8b479605a05b2cbb79a6b3470a86bc
Domenico Di Iorio <dome.diiorio@gmail.com>
2024-01-08 08:42:14 +0100

    WIP

--- sheets.js
+++ sheets.js
@@ -13,7 +13,9 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 const TOKEN_PATH = 'token.json';
 const JWT_PATH = 'jwt.keys.json';
 
-const SHEET_ID = '1FWHm8879jYwAspAvDS0eawJZK5dYt0kdxPLSdjY94yQ';
+//const SHEET_ID = '1FWHm8879jYwAspAvDS0eawJZK5dYt0kdxPLSdjY94yQ'; // Anna Domenico 5 settembre
+//const SHEET_ID = '14j53zk63XntBbG0LQ5LR9gq2K5DdZeJa4x0zhHDJ8V8';
+const SHEET_ID = '1-AzabjENPnJwW8wYL2vmxuyqrepv9rdmsU6Wg9N0HuE'; // Anna Domenico 28 Aprile
 
 const credentials = JSON.parse(fs.readFileSync('credentials.json'));
 const {client_secret, client_id, redirect_uris} = credentials.web;
@@ -43,17 +45,29 @@ function getById(auth, id, callback) {
   const sheets = google.sheets({version: 'v4', auth});
   sheets.spreadsheets.values.get({
     spreadsheetId: SHEET_ID,
-    range: 'Lista!A:D',
+    range: 'Lista!A:M',
   }, (err, rsp) => {
     if (err) {
       callback.onerror(err);
       return;
     }
+
     const rows = rsp.data.values;
     for (let i = 0; i < rows.length; ++i) {
       const row = rows[i];
       if (row[0] === id) {
-        filtered.push({id, name: row[1], checked: row[3] == 1, range: `D${i + 1}`});
+        filtered.push({
+          id,
+          name: row[1],
+          range: `D${i + 1}`,
+          line: `${i + 1}`,
+          checked: row[3]   == 1,
+          under5: row[5]    == 1,
+          bet5and10: row[6] == 1,
+          gluten: row[7]    == 1,
+          lactose: row[8]   == 1,
+          notes: row[9],
+        });
       }
     }
     callback.onsuccess(filtered);
@@ -77,10 +91,57 @@ function putByRange(auth, range, value, callback) {
   });
 }
 
+function putByLine(auth, line, data, callback) {
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.update({
+    spreadsheetId: SHEET_ID,
+    range: `Lista!D${line}:M${line}`,
+    resource: { values: [[
+      data.checked ? 1 : 0,
+      '',
+      data.under5 ? 1:0,
+      data.bet5adn10 ? 1:0,
+      data.gluten ? 1:0,
+      data.lactose ? 1:0,
+    ]] },
+    valueInputOption: 'USER_ENTERED'
+  }, (err, rsp) => {
+    if (err) {
+        callback.onerror(err);
+        return;
+    }
+    callback.onsuccess(data);
+  });
+}
+
+router.put('/line/:line', function(req, res, next) {
+  fs.readFile('credentials.json', (err, content) => {
+    authorize(JSON.parse(content), {
+      onsuccess: (auth) => {
+        putByLine(auth, `${req.params.line}`, {
+          checked: req.body.checked,
+          under5: false,
+          bet5and10: true,
+          gluten: false,
+          lactose: false,
+          notes: 'Prova',
+        }, {
+          onsuccess: (data) => {
+            res.json({message: `Updated D${req.params.line} as ${req.body.checked}`, data});
+          },
+          onerror: (err) => {
+            res.json({error: err})
+          }
+        });
+      }
+    });
+  });
+});
+
 router.put('/:id', function(req, res, next) {
   /**
    *  req.body = {
-   *    "name": "Rocco",
+   *    "range": "D234",
    *    "checked": true
    *  }
    */
